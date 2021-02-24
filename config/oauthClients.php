5d78aa70f5f5c8a5b751153ad3d956691fa87b41
Pawan Saxena <saxenapawan800@gmail.com>
2021-02-24 19:04:15 +0530

    Initial commit

---
+++ config/oauthClients.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'pm' => [
+        'clientId' => 'x-pm-local-client',
+        'clientSecret' => '123456789',
+        'clientName' => 'PM Web Designer',
+        'clientDescription' => 'ProcessMaker Web Designer App',
+        'clientWebsite' => 'www.processmaker.com',
+    ],
+    'mobile' => [
+        'clientId' => 'x-pm-mobile-client',
+        'clientSecret' => '123456789',
+        'clientName' => 'PM Mobile App',
+        'clientDescription' => 'PM Mobile App for Android and iOS',
+        'clientWebsite' => 'www.processmaker.com',
+    ],
+];
---
+++ workflow/engine/config/paths_installed.php
@@ -0,0 +1,5 @@
+<?php
+  define('PATH_DATA',         dirname(dirname(dirname(dirname(__FILE__)))) . '/shared/');
+  define('PATH_C',            dirname(dirname(dirname(dirname(__FILE__)))) . '/shared/compiled/');
+  define('HASH_INSTALLATION', 'abcdef');
+  define('SYSTEM_HASH',       'abcdef');
